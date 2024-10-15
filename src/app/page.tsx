8bbdb1ba59c28df8bbecc789bd5a930edbc229c0
StopSoo <jpy1030@naver.com>
2024-10-16 00:40:39 +0900

    feat: 태그 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function page() {
+import Tag from '@/components/common/Tag';
+
+export default function Home() {
   return (
     <>
-      <h1>Main page</h1>
+      <h1>main component</h1>
+      <div className='flex gap-2 mb-4'>
+        <Tag type="hot" />
+        <Tag type="best" />
+        <Tag type="new" />
+      </div>
     </>
   );
 }
