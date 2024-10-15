637832f27a5552afdd342589661373951a82a334
kitaePark <bkt4044@naver.com>
2024-10-15 20:06:48 +0900

    chore: 중복 코드 정리&Img alt 변경

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -19,31 +19,28 @@ export default function SearchBar({ placeholder }: { placeholder: string }) {
   return (
     <>
       <form onSubmit={onKeywordSearchHandler}>
-        {/* 토마토 아이콘 */}
-        <div className="w-[264px] h-10 px-2 py-1.5 rounded-[100px] border-2 border-[#e24444] flex justify-start items-center gap-[11px]">
+        {/* 토마토 이미지 */}
+        <div className="flex justify-center items-center w-[264px] h-10 px-2 py-1.5 rounded-[100px] border-2 border-searchBorder gap-[11px]">
           <div className="flex justify-center items-center">
             <Image
               src="/assets/common/PC_search_t.svg"
-              alt="PC_search_t"
+              alt="Tomato Icon"
               width={29}
               height={27.31}
             />
           </div>
-          {/* 검색 입력창 */}
+          {/* 검색창 */}
           <div className="flex-grow flex justify-between items-center">
             <input
               type="text"
               value={keyword}
               onChange={onKeywordChangeHandler}
               placeholder={placeholder}
-              className="text-[#4e4e4e] text-sm font-normal leading-[21px] bg-transparent outline-none"
+              className="text-[#4e4e4e] text-sm bg-transparent outline-none"
               style={{ fontFamily: 'Pretendard' }}
             />
-            {/* 검색 버튼 */}
-            <button
-              type="submit"
-              className="w-6 h-6 flex justify-center items-center"
-            >
+            {/* 버튼 */}
+            <button type="submit">
               <FaSearch className="text-searchBorder text-2xl" />
             </button>
           </div>
