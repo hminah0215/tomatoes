9204fee9c45d1815e051a23c1c60e417c650540d
StopSoo <jpy1030@naver.com>
2024-10-16 00:48:41 +0900

    refactor: 디데이 컴포넌트 반응형 수정

--- src/components/common/Dday.tsx
+++ src/components/common/Dday.tsx
@@ -4,8 +4,8 @@ interface DdayProps {
   color?: 'red' | 'yellow' | 'green';
 }
 
-const baseTagStyle = "h-[33px] px-2 py-[3px] rounded-[999px] justify-center items-center gap-1.5 inline-flex";
-const baseTextStyle = "text-lg font-medium font-pretendard leading-[27px]";
+const baseTagStyle = "h-[33px] px-2 py-[3px] rounded-[999px] justify-center items-center gap-1.5 inline-flex flex-wrap min-w-[85px] max-w-[120px] text-sm sm:text-sm md:text-base";
+const baseTextStyle = "font-medium font-pretendard leading-[27px] text-center";
 
 const tagStyles = {
   active: {
@@ -31,7 +31,11 @@ export default function Dday({ type, day, color }: DdayProps) {
   if (type === "active" && color) {
     return (
       <div className={tagStyles.active[color]}>
-        <img src={`/assets/common/Plain_${color}_t.svg`} alt="Icon" className="w-6 h-6" />
+        <img 
+          src={`/assets/common/Plain_${color}_t.svg`} 
+          alt="Icon" 
+          className={`min-w-[24px] min-h-[24px]`}
+        />
         <span className={textStyles.active[color]}>
           D-{day}
         </span>
@@ -40,7 +44,11 @@ export default function Dday({ type, day, color }: DdayProps) {
   } else if (type === "completed") {
     return (
       <div className={tagStyles['completed']}>
-        <img src="/assets/common/Plain_gray_t.svg" alt="Icon" className="w-6 h-6" />
+        <img 
+          src="/assets/common/Plain_gray_t.svg" 
+          alt="Icon" 
+          className={`min-w-[24px] min-h-[24px]`}
+        />
         <span className={textStyles['completed']}>
           마감
         </span>
@@ -49,7 +57,11 @@ export default function Dday({ type, day, color }: DdayProps) {
   } else {  // upcoming
     return (
       <div className={tagStyles['upcoming']}>
-        <img src="/assets/common/Plain_green_t.svg" alt="Icon" className="w-6 h-6" />
+        <img 
+          src="/assets/common/Plain_green_t.svg" 
+          alt="Icon" 
+          className={`min-w-[24px] min-h-[24px]`}
+        />
         <span className={textStyles['upcoming']}>
           예정
         </span>
