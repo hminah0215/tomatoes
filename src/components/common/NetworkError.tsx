111e0a1f5b705f256b4fce4120e215b58012f86f
kitaePark <bkt4044@naver.com>
2024-10-17 01:40:25 +0900

    design: btn CSS 수정

--- src/components/common/NetworkError.tsx
+++ src/components/common/NetworkError.tsx
@@ -23,14 +23,14 @@ export default function NetworkError() {
           unoptimized={true}
         />
       </div>
-      <p className="text-sub-gray-500 text-2xl font-semibold leading-9 mb-[14px]">
+      <p className="text-sub-gray-500 text-2xl font-semibold mb-[14px]">
         인터넷 연결을 확인하세요.
       </p>
-      <p className="text-center text-sub-gray-500 text-base font-normal leading-normal mb-[40px]">
+      <p className="text-center text-sub-gray-500 text-base font-normal mb-[40px]">
         서버와의 통신이 원활하지 않아 데이터를 불러올 수 없습니다.
       </p>
       <button
-        className="w-[168px] h-[46px] px-4 py-2 bg-point-red-500 rounded-md flex justify-center items-center gap-2.5 text-white text-xl font-medium leading-[30px] font-['Pretendard']"
+        className="w-[168px] h-[46px] px-4 py-2 bg-point-red-500 rounded-md text-white text-xl font-medium"
         onClick={() => window.location.reload()}
       >
         다시 시도
