16eefc0f15a8ea9c16d1d86e21d1f58f78ea6e89
kitaePark <bkt4044@naver.com>
2024-10-17 02:04:20 +0900

    chore: page.tsx 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import NetworkError from '@/components/common/NetworkError';
+import Dday from '@/components/common/Dday';
+import Tag from '@/components/common/Tag';
 
 export default function Home() {
   return (
     <>
-      <NetworkError />
+      <h1>main component</h1>
+      <div className="flex gap-2 mb-4">
+        <Tag type="hot" label="HOT" />
+        <Tag type="best" label="BEST" />
+        <Tag type="new" label="NEW" />
+      </div>
+      <div className="flex gap-2 mb-4">
+        <Dday type="active" day="30" color="red" />
+        <Dday type="active" day="00" color="green" />
+        <Dday type="active" day="00" color="yellow" />
+        <Dday type="upcoming" />
+        <Dday type="completed" />
+      </div>
     </>
   );
 }
