041da9952aa32f2d9c84322e2c855d3de37afade
StopSoo <jpy1030@naver.com>
2024-10-16 17:58:10 +0900

    design: CSS 배치 수정

--- src/components/noResult.tsx
+++ src/components/noResult.tsx
@@ -1,28 +1,34 @@
+import { AiOutlineClose } from 'react-icons/ai';
+
 export default function NoResult() {
   let keyword = "토마토들";
   let count = 0;
 
   return (
-    <>
-      <button>X</button>
-      <div>
+    <div className="relative w-full h-screen flex flex-col items-center">
+      <button className="absolute top-12 left-12">
+        <AiOutlineClose className="w-11 h-11" />
+      </button>
+      <div className="mt-28 px-12 flex flex-col items-start justify-start w-full ">
         <img 
           src="/assets/noResult/PC_header_t.svg"
           alt="Header"
-          className="h-[67.80px] justify-start items-center gap-6 inline-flex"
+          className="h-[67.80px] mb-4"
         />
+        <p><span className="text-point-red-500">{`'${keyword}'`}</span>에 대한 검색 결과 총 {`${count}건`}</p>
       </div>
-      <p><span className="text-point-red-500">{`'${keyword}'`}</span>에 대한 검색 결과 총 {`${count}건`}</p>
-      <div className="font-pretendard">
-        <img 
-          src="/assets/noResult/PC_logo_t.svg"
-          alt="Logo"
-          className="w-[225px] h-[189px] relative"
-        />
-        <span className="text-[30px] font-semibold font-pretendard leading-[60px]">검색 결과가 없습니다.<br/></span>
-        <p><span className="text-point-red-500">{`'${keyword}'`}</span>에 대한 검색 결과가 없습니다.</p>
-        <p>{`다른 검색어 또는 추천 검색어를 활용하여 검색해보세요.`}</p>
+      <div className="flex flex-col items-center justify-center h-full">
+        <div className="font-pretendard flex flex-col items-center">
+          <img 
+            src="/assets/noResult/PC_logo_t.svg"
+            alt="Logo"
+            className="w-[225px] h-[189px] relative"
+          />
+          <span className="text-[30px] font-semibold font-pretendard leading-[60px] mb-4">검색 결과가 없습니다.<br/></span>
+          <p><span className="text-point-red-500">{`'${keyword}'`}</span>에 대한 검색 결과가 없습니다.</p>
+          <p>{`다른 검색어 또는 추천 검색어를 활용하여 검색해보세요.`}</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
