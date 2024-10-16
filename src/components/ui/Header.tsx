60eeab8e6e0f22fc033597e8495c944859ce673e
starcradle101 <dreamerdev12@gmail.com>
2024-10-16 10:54:22 +0900

    design: 헤더 및 내비게이션 컴포넌트 레이아웃 구현
    Related to: #7

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,31 @@
+import Navigation from './Navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className="flex items-center px-7 py-4 lg:px-[88px] lg:py-[22px] justify-between w-full shadow">
+      <Link href="/" className="mr-8 shrink-0">
+        {/* 데스크탑 로고 */}
+        <Image
+          src="/assets/common/PC_logo_text (1).svg"
+          width={160}
+          height={30}
+          alt="tomatoes desktop logo"
+          className="hidden md:block"
+        />
+
+        {/* 모바일 로고 */}
+        <Image
+          src="/assets/common/MO_logo_text.svg"
+          width={76}
+          height={17}
+          alt="tomatoes mobile logo"
+          className="md:hidden block"
+        />
+      </Link>
+
+      <Navigation />
+    </header>
+  );
+}
