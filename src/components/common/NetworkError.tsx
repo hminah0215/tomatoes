613a3d13508c7607b1f8d2e6fe7d6b823165c519
kitaePark <bkt4044@naver.com>
2024-10-17 01:33:50 +0900

    design: CSS 작업&마진 설정

--- src/components/common/NetworkError.tsx
+++ src/components/common/NetworkError.tsx
@@ -1,23 +1,41 @@
 'use client';
-import React from 'react';
+
+import Image from 'next/image';
 
 export default function NetworkError() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      {/* 토마토 이미지 */}
-      <img
-        src="/assets/common/PC_net_err_t.svg"
-        alt="토마토 이미지"
-        className="w-24 h-24"
-      />
-      {/* 와이파이 이미지 */}
-      <img
-        src="/assets/common/PC_net_err_wifi.svg"
-        alt="와이파이 신호 이미지"
-      />
-      <p>인터넷 연결을 확인하세요.</p>
-      <p>서버와의 통신이 원활하지 않아 데이터를 불러올 수 없습니다.</p>
-      <button onClick={() => window.location.reload()}>다시 시도</button>
+      <div className="relative w-[92px] h-[90.49px] mb-[30.51px]">
+        {/* 토마토 이미지 */}
+        <Image
+          src="/assets/common/PC_net_err_t.svg"
+          alt="Tomato Icon"
+          width={92}
+          height={90.49}
+          unoptimized={true}
+        />
+        {/* 와이파이 이미지 */}
+        <Image
+          src="/assets/common/PC_net_err_wifi.svg"
+          alt="Wifi Icon"
+          width={42.5}
+          height={32.5}
+          className="absolute top-[-15px] right-[-20px]"
+          unoptimized={true}
+        />
+      </div>
+      <p className="text-sub-gray-500 text-2xl font-semibold leading-9 mb-[14px]">
+        인터넷 연결을 확인하세요.
+      </p>
+      <p className="text-center text-sub-gray-500 text-base font-normal leading-normal mb-[40px]">
+        서버와의 통신이 원활하지 않아 데이터를 불러올 수 없습니다.
+      </p>
+      <button
+        className="w-[168px] h-[46px] px-4 py-2 bg-point-red-500 rounded-md flex justify-center items-center gap-2.5 text-white text-xl font-medium leading-[30px] font-['Pretendard']"
+        onClick={() => window.location.reload()}
+      >
+        다시 시도
+      </button>
     </div>
   );
 }
