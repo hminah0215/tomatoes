7060e4d78a20612f399f89525192d016709af59e
StopSoo <jpy1030@naver.com>
2024-10-16 21:09:27 +0900

    style, docs: 주석 및 변수 수정

--- src/components/noResult.tsx
+++ src/components/noResult.tsx
@@ -1,8 +1,7 @@
 import { AiOutlineClose } from 'react-icons/ai';
 
 export default function NoResult() {
-  let keyword = "토마토들"; // 사용자가 입력한 키워드
-  let count = 0;  // 검색 결과 수
+  let keyword = "토마토들"; // 사용자가 입력한 키워드, 추후 수정 예정
 
   return (
     <div className="relative w-full h-screen flex flex-col items-center">
@@ -16,7 +15,7 @@ export default function NoResult() {
           className="h-[50px] mb-4 md:h-[50px] lg:h-[55px]"
         />
         <p>
-          <span className="text-point-red-500">{`'${keyword}'`}</span> 에 대한 검색 결과 총 {`${count} 건`}
+          <span className="text-point-red-500">{`'${keyword}'`}</span> 에 대한 검색 결과 총 0건
         </p>
       </div>
       <div className="flex flex-col items-center justify-center h-full">
@@ -27,7 +26,7 @@ export default function NoResult() {
             className="relative mt-28 w-[150px] h-[120px] md:w-[200px] md:h-[160px] lg:w-[225px] lg:h-[189px]"
           />
           <p className="text-[22px] font-semibold font-pretendard leading-[60px] mt-4 mb-4 md:text-[25px] lg:text-[30px]">
-            검색 결과가 없습니다.<br/>
+            검색 결과가 없습니다.
           </p>
           <p className="text-center">
             <span className="text-point-red-500">{`'${keyword}'`}</span> 에 대한 검색 결과가 없습니다.
