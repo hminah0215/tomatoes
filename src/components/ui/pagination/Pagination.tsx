b2f80d673f231a94af6636db4aad66bc0a6da310
starcradle101 <dreamerdev12@gmail.com>
2024-10-17 13:03:24 +0900

    feat: 페이지네이션 컴포넌트 구현
    - Pagination
    - PaginationControl
    Related to: #13

---
+++ src/components/ui/pagination/Pagination.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import GridView from '../grid/GridView';
+import PaginationControl from './PaginationControl';
+
+function Pagination({ contents = [] }: { contents: Activities }) {
+  // 기본값 빈 배열
+  const [currentPage, setCurrentPage] = useState(1);
+  const pathname = usePathname();
+
+  // 경로에 따라 한 페이지당 아이템 개수를 설정
+  const itemsPerPage = ['/activity', '/contest', '/talk'].includes(pathname)
+    ? 16
+    : 8;
+
+  const totalItems = contents.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  // 현재 페이지에 표시될 컨텐츠 계산
+  const currentItems = contents.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (page: number) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  return (
+    <section className="flex flex-col gap-4 md:gap-6">
+      <span className="text-sm md:text-xl md:font-medium text-sub-gray-200">
+        검색결과 {contents.length}건
+      </span>
+      <GridView activities={currentItems} />
+      <PaginationControl
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
+    </section>
+  );
+}
+
+export default Pagination;
