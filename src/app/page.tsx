a679d3b6c4ea45e7e1232e36c929d666b269f40b
StopSoo <jpy1030@naver.com>
2024-10-21 22:27:19 +0900

    style: asset 파일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>main component</h1>
-      <div className="flex gap-2 mb-4">
-        <Tag type="hot" label="HOT" />
-        <Tag type="best" label="BEST" />
-        <Tag type="new" label="NEW" />
-      </div>
-      <div className="flex gap-2 mb-4">
-        <Dday type="active" day="30" color="red" />
-        <Dday type="active" day="00" color="green" />
-        <Dday type="active" day="00" color="yellow" />
-        <Dday type="upcoming" />
-        <Dday type="completed" />
-      </div>
+      {/* 스와이프 공고 */}
       <Pagination contents={dummyActivities} />
     </>
   );
