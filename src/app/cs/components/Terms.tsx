6dde7766d5b1aa88f20cc93f1d6e058e4bc2e294
kitaePark <bkt4044@naver.com>
2024-10-23 00:54:41 +0900

    chore: 사업소개 이외 나머지 메뉴에 제목 추가

--- src/app/cs/components/Terms.tsx
+++ src/app/cs/components/Terms.tsx
@@ -1,3 +1,15 @@
 export default function Terms() {
-  return <div>terms</div>;
+  return (
+    <>
+      <div>
+        {/* 상단 타이틀 */}
+        <div className="h-[72px] text-sub-gray-500 text-5xl font-bold mt-[88px]">
+          이용약관
+        </div>
+
+        {/* 구분선 */}
+        <div className="w-[999px] h-[1px] bg-sub-gray-200 mt-4 mb-[40px]" />
+      </div>
+    </>
+  );
 }
