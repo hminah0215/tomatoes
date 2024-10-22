630594e8acadca56b99c83bbe0bae76623a2d10a
hminah0215 <hyeonminah@gmail.com>
2024-10-23 05:23:47 +0900

    feat: 토마토 pick 지금꼭봐야하는매거진까지 추가 [#24]

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -1,13 +1,22 @@
 // src/app/magazine/page.tsx
 
-import MagazineHeader from './components/MagazineHeader';
+import Image from 'next/image';
+
 import CurrentHighlights from './components/CurrentHighlights';
 import MagazineList from './components/MagazineList';
 
 const MagazinePage = () => {
   return (
-    <div className="relative bg-white">
-      <MagazineHeader />
+    <div className="bg-white ml-[88px] mr-[88px]">
+      {/* 배너 이미지 추가 */}
+      <div className="relative w-full max-w-[1264px] mx-auto mt-[66px] mb-[106px]">
+        <Image
+          src="/assets/magazine/PC_magazine_banner.svg"
+          alt="매거진 배너"
+          width={1264}
+          height={478}
+        />
+      </div>
       <CurrentHighlights />
       <MagazineList />
     </div>
