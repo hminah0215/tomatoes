e992488a75b1c195c1b762daeb42f54750952ee1
starcradle101 <dreamerdev12@gmail.com>
2024-10-23 12:46:16 +0900

    chore: tailwind.config.ts 수정
    * main-white 색상 추가
    Related to: #20

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         // sub: 일반적으로 배경, 텍스트, 섹션 구분 등에 사용. 눈에 덜 띄며 시각적 안정성
         // point: 주로 강조하고 싶은 요소나 중요한 요소에 사용. 버튼, 강조된 텍스트 등
         // 끝에 붙는 숫자: 밝기 또는 채도를 의미하며, 숫자가 커질수록 진하고 어두운 색상.
+        'main-white': '#FFFFFF',
         'main-beige': '#F5F1DE',
         'point-red-100': '#F9DADA',
         'point-red-500': '#E24444',
@@ -28,8 +29,8 @@ const config: Config = {
         'sub-gray-500': '#222222',
       },
       fontFamily: {
-        recipe: ['Recipekorea', 'Pretendard', 'sans-serif']
-      }
+        recipe: ['Recipekorea', 'Pretendard', 'sans-serif'],
+      },
     },
   },
   plugins: [],
