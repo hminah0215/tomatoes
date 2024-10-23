544125ffcf4610f2dd7a93fce94a1efd8e2534dc
kitaePark <bkt4044@naver.com>
2024-10-23 12:16:21 +0900

    design: Intro 반응형 작업중 커밋(추후 진행)

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -6,27 +6,31 @@ export default function IntroPage() {
       <Image
         src="/assets/intro/PC_banner (1).svg"
         alt="Tomato Banner"
-        width={1440}
-        height={586}
+        width={375}
+        height={294}
+        className="md:w-[1440px] md:h-[586px]"
       />
-      <div className="grid grid-cols-3 rounded-md gap-4 mx-[88px] my-[120px]">
+      <div className="grid gird-cols-1 w-[375px] md:grid-cols-3 md:w-[1264px] rounded-md gap-4 mx-[88px] my-[120px]">
         <Image
           src="/assets/intro/PC_grid_t (1).svg"
           alt="Tomato Grid"
-          width={411}
-          height={568}
+          width={375}
+          height={168}
+          className="md:w-[411px] md:h-[568px]"
         />
         <Image
           src="/assets/intro/PC_grid_t (2).svg"
           alt="Tomato Grid"
-          width={411}
-          height={568}
+          width={375}
+          height={168}
+          className="md:w-[411px] md:h-[568px]"
         />
         <Image
           src="/assets/intro/PC_grid_t (3).svg"
           alt="Tomato Grid"
-          width={411}
-          height={568}
+          width={375}
+          height={168}
+          className="md:w-[411px] md:h-[568px]"
         />
       </div>
       <Image
