eebc3601d84de89c41af2dba14c451ebf0451df7
starcradle101 <dreamerdev12@gmail.com>
2024-10-28 10:42:16 +0900

    refactor: 페이지네이션 관련 컴포넌트 구현
    Related to: #35

--- src/components/ui/grid/GridViewTest.tsx
+++ src/components/ui/grid/GridViewTest.tsx
@@ -1,11 +1,32 @@
+/**
+ * GridViewTest 컴포넌트는 주어진 항목을 그리드 레이아웃으로 렌더링합니다.
+ *
+ * @template T 항목의 타입을 나타냅니다.
+ * @param {T[]} items - 렌더링할 항목들의 배열입니다.
+ * @param {React.ComponentType<{ item: T }>} GridItem - 각 항목을 렌더링할 컴포넌트입니다.
+ * @param {{ mobile: number, web: number }} columns - 모바일 및 웹에서의 그리드 열 수를 정의합니다.
+ * @param {GapStyles} gapStyle - 그리드 아이템 간의 간격 스타일을 선택합니다.
+ *    - 'gapStyle1': BEST PICK, 토마토들 추천활동, 토마토 Pick에 사용됩니다.
+ *    - 'gapStyle2': 대외활동, 공모전에 사용됩니다.
+ *    - 'gapStyle3': 토마토 Tip에 사용됩니다.
+ *
+ * @returns {JSX.Element} 그리드 레이아웃으로 렌더링된 항목들을 반환합니다.
+ */
+
 export default function GridViewTest<T>({
   items,
   GridItem,
-  columns,
-  variant,
+  columnStyle,
+  gapStyle,
 }: GridViewProps<T>) {
   // 커스텀 gap 정의
-  const gapVariants = {
+
+  const columnStyles = {
+    web4mobile2: { mobile: 2, web: 4 },
+    web3mobile1: { mobile: 1, web: 3 },
+  };
+
+  const gapStyles = {
     gapStyle1: 'gap-x-14 gap-y-40 md:gap-x-20 md:gap-y-80', // BEST PICK, 토마토들 추천활동, 토마토 Pick
     gapStyle2: 'gap-x-14 gap-y-32 md:gap-x-20 md:gap-y-64', // 대외활동, 공모전
     gapStyle3: 'gap-x-0 gap-y-16 md:gap-x-30 md:gap-y-80', // 토마토 Tip
@@ -13,9 +34,9 @@ export default function GridViewTest<T>({
 
   const gridClass = `
     grid 
-    grid-cols-${columns.mobile} 
-    md:grid-cols-${columns.web} 
-    ${gapVariants[variant]}
+    grid-cols-${columnStyles[columnStyle].mobile} 
+    md:grid-cols-${columnStyles[columnStyle].web} 
+    ${gapStyles[gapStyle]}
   `.trim();
 
   return (
--- src/containers/home/HomeGridItem.tsx
+++ src/containers/home/HomeGridItem.tsx
@@ -1,16 +1,19 @@
 'use client';
 import Image from 'next/image';
 import Dday from '@/components/common/Dday';
-import { formatViewCount } from '@/utils/format';
 import Link from 'next/link';
 
-export default function ActivityContestItem({
-  item,
-}: ActivityContestListProps) {
-  const { title, organization, dDay, viewCount, imageUrl, detailUrl } = item;
+// 접수 기간의 마감 날짜만 추출하는 함수
+const extractEndDate = (receptionPeriod: string) => {
+  const dates = receptionPeriod.split('~');
+  return dates[1]?.trim() || receptionPeriod; // 마감 날짜가 없으면 전체 기간 반환
+};
+
+export default function HomeGridItem({ item }: ActivityContestListProps) {
+  const { imageUrl, title, dDay, receptionPeriod, category, detailUrl } = item;
 
   return (
-    <Link href={detailUrl} className="block justify-self-start">
+    <Link href={detailUrl} className="relative block justify-self-start">
       {/* 모바일 이미지 */}
       <div className="block sm:hidden">
         <Image
@@ -33,20 +36,23 @@ export default function ActivityContestItem({
         />
       </div>
 
-      {/* 제목, 주최기관, 디데이 및 조회수 */}
+      {/* 카테고리 */}
+      <div className="absolute right-2 top-2 rounded-full bg-sub-gray-500 px-2 py-[2px] text-xs font-normal text-white md:right-3 md:top-3 md:px-3 md:py-1 md:text-xl md:font-medium">
+        {category}
+      </div>
+
       <div className="mt-2 md:mt-4">
+        {/* 제목 */}
         <h2 className="line-clamp-2 h-12 text-base font-semibold md:h-[78px] md:text-xl md:font-semibold">
           {title}
         </h2>
 
-        <p className="mb-2 text-sm font-normal text-sub-gray-400 md:text-xl md:font-medium">
-          {organization}
-        </p>
-
+        {/* D-Day 및 접수 기간 */}
         <div className="flex items-center gap-1.5">
           <Dday type="active" day={dDay} color="red" />
           <p className="md:text-md text-xs font-normal text-sub-gray-300 sm:text-sm md:font-medium lg:text-base xl:text-base">
-            조회 {formatViewCount(viewCount)}회
+            <span className="inline sm:hidden">{`~ ${extractEndDate(receptionPeriod)}`}</span>
+            <span className="hidden sm:inline">{receptionPeriod}</span>
           </p>
         </div>
       </div>
