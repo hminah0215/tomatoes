2a9a366c08867e0ea53fbf4fbbfabbd6ad241d94
starcradle101 <dreamerdev12@gmail.com>
2024-10-28 10:57:19 +0900

    comment: GridViewTest JSDoc 수정
    Related to: #35

--- src/components/ui/grid/GridViewTest.tsx
+++ src/components/ui/grid/GridViewTest.tsx
@@ -2,10 +2,13 @@
  * GridViewTest 컴포넌트는 주어진 항목을 그리드 레이아웃으로 렌더링합니다.
  *
  * @template T 항목의 타입을 나타냅니다.
- * @param {T[]} items - 렌더링할 항목들의 배열입니다.
- * @param {React.ComponentType<{ item: T }>} GridItem - 각 항목을 렌더링할 컴포넌트입니다.
- * @param {{ mobile: number, web: number }} columns - 모바일 및 웹에서의 그리드 열 수를 정의합니다.
- * @param {GapStyles} gapStyle - 그리드 아이템 간의 간격 스타일을 선택합니다.
+ * @param {Object} props - 컴포넌트의 속성입니다.
+ * @param {T[]} props.items - 렌더링할 항목들의 배열입니다.
+ * @param {React.ComponentType<{ item: T }>} props.GridItem - 각 항목을 렌더링할 컴포넌트입니다.
+ * @param {'web4mobile2' | 'web3mobile1'} props.columnStyle - 모바일 및 웹에서의 그리드 열 수를 정의하는 스타일입니다.
+ *    - 'web4mobile2': 모바일에서 2열, 웹에서 4열 그리드
+ *    - 'web3mobile1': 모바일에서 1열, 웹에서 3열 그리드
+ * @param {'gapStyle1' | 'gapStyle2' | 'gapStyle3'} props.gapStyle - 그리드 항목 간의 간격 스타일입니다.
  *    - 'gapStyle1': BEST PICK, 토마토들 추천활동, 토마토 Pick에 사용됩니다.
  *    - 'gapStyle2': 대외활동, 공모전에 사용됩니다.
  *    - 'gapStyle3': 토마토 Tip에 사용됩니다.
