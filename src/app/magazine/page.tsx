1951c43a6ec0e3baf5493802e6c60afe1ad8b6ff
hminah0215 <hyeonminah@gmail.com>
2024-10-29 01:55:53 +0900

    design: 못하겠다 난 잘래 #45

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -9,12 +9,20 @@ const MagazinePage = () => {
   return (
     <div className="bg-white">
       {/* 배너 이미지 추가 */}
-      <div className="relative mb-[106px] mt-[66px] h-full w-full">
+      <div className="mb-[42px] mt-[10px] h-full w-full md:mx-[88px] md:mb-[106px] md:mt-[66px]">
         <Image
           src="/assets/magazine/PC_magazine_banner.svg"
-          alt="매거진 배너"
+          alt="매거진 PC 배너"
           width={1264}
           height={478}
+          className="hidden object-cover md:block"
+        />
+        <Image
+          src="/assets/magazine/MO_magazine_banner.svg"
+          alt="매거진 MO 배너"
+          width={393.51}
+          height={148.81}
+          className="block w-full object-cover md:hidden"
         />
       </div>
       <CurrentHighlights />
