8cd24979b6bed6592d6f8f8e34158f0486766e6f
starcradle101 <dreamerdev12@gmail.com>
2024-10-28 10:41:40 +0900

    feat: 타입 수정
    Related to: #35

--- src/types/grid.d.ts
+++ src/types/grid.d.ts
@@ -1,30 +1,39 @@
-// grid와 관련된 타입 정의
+// 그리드 열 수 정의
+type ColumnStyles = 'web4mobile2' | 'web3mobile1';
 
-type GapVariant = 'gapStyle1' | 'gapStyle2' | 'gapStyle3';
+type GapStyles = 'gapStyle1' | 'gapStyle2' | 'gapStyle3';
+
+type PaginationTestProps<T> = {
+  contents: T[];
+  GridItem: React.ComponentType<GridItemProps<T>>;
+  webItemPerPage: number;
+  mobileItemPerPage: number;
+  columnStyle: GridColumns; // 그리드 열 수를 미리 약속된 값으로 지정
+  gapStyle: GapStyles; // 그리드 스타일
+};
 
 type GridViewProps<T> = {
   items: T[];
   GridItem: React.ComponentType<{ item: T }>;
-  columns: {
-    mobile: number;
-    web: number;
-  };
-  variant: GapVariant;
+  columnStyle: ColumnStyles; // 그리드 열 수를 미리 약속된 값으로 지정
+  gapStyle: GapStyles;
 };
 
-type ActivityContestProps = {
+type GridItemProps<T> = {
+  item: T;
+};
+
+type ActivityContestDetailsProps = {
   title: string;
   organization: string;
   dDay: string;
-  viewCount: string;
+  receptionPeriod: string; // 접수 기간
+  category: '대외활동' | '공모전'; // 카테고리 추가
+  viewCount: string; // 조회수 추가
   imageUrl: string;
   detailUrl: string;
 };
 
-type ActivityContestGridItemProps = {
-  item: ActivityContestProps;
+type ActivityContestListProps = {
+  item: ActivityContestDetailsProps;
 };
-
-type ActivityCategoryProps = {};
-
-type ContestCategoryProps = {};
