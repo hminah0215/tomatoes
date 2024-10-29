05916770e8856ff0d9aa1fe19f2209601a775cd0
hminah0215 <hyeonminah@gmail.com>
2024-10-29 18:41:51 +0900

    feat : 그리드슬라이드뷰 컴포넌트 생성 #45

--- src/types/grid.d.ts
+++ src/types/grid.d.ts
@@ -3,6 +3,10 @@ type ColumnStyles = 'web4mobile2' | 'web3mobile1';
 
 type GapStyles = 'gapStyle1' | 'gapStyle2' | 'gapStyle3';
 
+type SlideColumnStyles = 'web3mobile3' | 'web6mobile6';
+
+type SlideGaptyles = 'slideGapStyle1' | 'slideGapStyle2';
+
 type PaginationProps<T> = {
   contents: T[];
   GridItem: React.ComponentType<GridItemProps<T>>;
@@ -18,6 +22,14 @@ type GridViewProps<T> = {
   columnStyle: ColumnStyles; // 그리드 열 수를 미리 약속된 값으로 지정
   gapStyle: GapStyles;
 };
+
+// 그리드 슬라이드뷰
+interface GridSlideViewProps<T> {
+  items: T[];
+  GridItem: React.ComponentType<{ item: T }>;
+  slideColumnStyle: SlideColumnStyles;
+  slideGapStyle: SlideGaptyles;
+}
 
 type GridItemProps<T> = {
   item: T;
