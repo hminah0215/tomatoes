6b4d3d99e9f431571f3c353e5d63b9c971a33312
starcradle101 <dreamerdev12@gmail.com>
2024-10-29 23:50:40 +0900

    comment: 공모전 상세페이지 임시 주석처리
    Related to: #49

--- src/containers/activity/ActivityDetail.tsx
+++ src/containers/activity/ActivityDetail.tsx
@@ -1,43 +1,43 @@
-import Image from 'next/image';
-import Dday from '@/components/common/Dday';
+// import Image from 'next/image';
+// import Dday from '@/components/common/Dday';
 
-export default function ActivityDetail({item}: ) {
-  return (
-    <section>
-      <div>
-        <div>
-          <Image src={} alt="" width={} height={} className="block sm:hidden" />
-          <Image src={} alt="" width={} height={} className="hidden sm:block" />
-        </div>
-        <div>
-          <Dday type="" day="" color="" />
-          <h2></h2>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">등록일: </span>
-            <span>조회 회</span>
-          </div>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">공모 분야</span>
-            <span></span>
-          </div>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">공모 기간</span>
-            <span></span>
-          </div>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">주최 기관</span>
-            <span></span>
-          </div>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">참여 대상</span>
-            <span></span>
-          </div>
-          <div>
-            <span className="pr-[30px] md:pr-[50px]">이지</span>
-            <span></span>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
+// export default function ActivityDetail({item}: ) {
+//   return (
+//     <section>
+//       <div>
+//         <div>
+//           <Image src={} alt="" width={} height={} className="block sm:hidden" />
+//           <Image src={} alt="" width={} height={} className="hidden sm:block" />
+//         </div>
+//         <div>
+//           <Dday type="" day="" color="" />
+//           <h2></h2>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">등록일: </span>
+//             <span>조회 회</span>
+//           </div>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">공모 분야</span>
+//             <span></span>
+//           </div>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">공모 기간</span>
+//             <span></span>
+//           </div>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">주최 기관</span>
+//             <span></span>
+//           </div>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">참여 대상</span>
+//             <span></span>
+//           </div>
+//           <div>
+//             <span className="pr-[30px] md:pr-[50px]">이지</span>
+//             <span></span>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
