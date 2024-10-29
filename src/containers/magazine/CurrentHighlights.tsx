e6cd29c9b39f4a0bbd452857cb70c12640d43764
hminah0215 <hyeonminah@gmail.com>
2024-10-29 18:42:24 +0900

    feat : 지금꼭봐야하는매거진에 그리드슬라이드뷰 컴포넌트 적용 #45

--- src/containers/magazine/CurrentHighlights.tsx
+++ src/containers/magazine/CurrentHighlights.tsx
@@ -1,13 +1,47 @@
-import TomatoTips from './TomatoTips';
+'use client';
+
+import { TomatoTipDataType } from '@/types/tomatoTips';
+import TomatoTipItem from '@/containers/magazine/TomatoTipItem';
+import { useEffect, useState } from 'react';
+import { fetchThreeTomatoTips } from '@/lib/fetchTomatoTip';
+import GridSlideView from '@/components/ui/grid/GridSlideView';
 
 const CurrentHighlights = () => {
+  const [tips, setTips] = useState<TomatoTipDataType[]>([]);
+
+  useEffect(() => {
+    const fetchTips = async () => {
+      // 필요한 3개의 데이터만 가져오는 함수 불러서 쓰기
+      const { data, error } = await fetchThreeTomatoTips();
+
+      if (error) {
+        console.error('Error fetching tips:', error);
+        return;
+      }
+
+      if (data) {
+        setTips(data);
+      }
+    };
+
+    fetchTips();
+  }, []);
+
   return (
-    <div className="mx-auto">
-      <div className="font-['Recipekorea'] text-[32px] font-normal leading-[48px] text-sub-gray-500">
+    <div className="mb-[42px] mt-[10px] h-full w-full md:mx-[88px] md:mt-[106px]">
+      <div className="font-['Recipekorea'] text-[20px] font-medium text-sub-gray-500 md:text-[32px]">
         지금 꼭 봐야하는 매거진
       </div>
-
-      <TomatoTips pageSize={3} showPagination={false} />
+      <div className="mb-[72px] mt-[28px] flex items-center md:mb-[120px] md:mt-[40px] md:items-start">
+        {tips.length > 0 && ( // tips가 비어 있지 않을 때만 GridSlideView 렌더링
+          <GridSlideView<TomatoTipDataType>
+            items={tips}
+            GridItem={TomatoTipItem}
+            slideColumnStyle="web3mobile3"
+            slideGapStyle="slideGapStyle1"
+          />
+        )}
+      </div>
     </div>
   );
 };
