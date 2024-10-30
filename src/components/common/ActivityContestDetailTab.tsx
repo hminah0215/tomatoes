1930a2d219adcb6a7357e83a840f28a0432c36a1
starcradle101 <dreamerdev12@gmail.com>
2024-10-31 02:29:27 +0900

    feat: 추가 css 수정
    Related to: #61

--- src/components/common/ActivityContestDetailTab.tsx
+++ src/components/common/ActivityContestDetailTab.tsx
@@ -10,14 +10,14 @@ export default function ActivityContestDetailTab({
   const tabs = ['상세내용', '토마토 TIP', '수상작 갤러리'];
 
   return (
-    <ul className="mb-16 mt-40 flex items-center justify-center border-b-[1px] border-sub-gray-100">
+    <ul className="mb-16 mt-16 flex items-center justify-center border-b-[1px] border-sub-gray-100 md:mt-40">
       {tabs.map((tab) => (
         <li
           key={tab}
           onClick={() => setActiveTab(tab)}
-          className={`flex-1 cursor-pointer pb-[18px] text-center text-2xl font-semibold ${
+          className={`flex-1 cursor-pointer pb-[18px] text-center text-base font-medium md:text-2xl md:font-semibold ${
             activeTab === tab
-              ? 'border-b-4 border-point-red-500 text-point-red-500'
+              ? 'border-b-2 border-point-red-500 text-point-red-500 md:border-b-4'
               : 'text-sub-gray-200'
           }`}
         >
