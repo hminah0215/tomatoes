fdd99f7b778670cb39dabc9ebb77e0fdcc06f1fd
hminah0215 <hyeonminah@gmail.com>
2024-10-30 11:55:10 +0900

    chore : 데이터 호출 함수 추가 #51

--- src/lib/fetchBestPickGridView.ts
+++ src/lib/fetchBestPickGridView.ts
@@ -1,4 +1,5 @@
 import { supabase } from '@/lib/supabaseClient';
+import { PostgrestError } from '@supabase/supabase-js';
 
 export const fetchBestPickActivities = async () => {
   const { data, error } = await supabase
@@ -6,6 +7,23 @@ export const fetchBestPickActivities = async () => {
     .select('*')
     .order('view_count', { ascending: false })
     .limit(8);
+
+  return { data, error };
+};
+
+// 전체 데이터를 가져오는 함수
+export const fetchBestPickAll = async (): Promise<{
+  data: ContestActivityDataProps[] | null;
+  error: PostgrestError | null;
+}> => {
+  const { data, error } = await supabase
+    .from('activities_contests')
+    .select('*')
+    .order('view_count', { ascending: false });
+
+  if (error) {
+    console.error('fetchBestPickAll 데이터 가져오기 실패:', error);
+  }
 
   return { data, error };
 };
