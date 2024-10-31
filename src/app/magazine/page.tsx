39da1413094ec190797d8d75ebe841cb34fc789d
hminah0215 <hyeonminah@gmail.com>
2024-10-31 18:22:57 +0900

    dsign :매거진 배너 css 수정

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -9,13 +9,13 @@ const MagazinePage = () => {
     <>
       <div className="bg-white">
         {/* 배너 이미지 추가 */}
-        <div className="px-[28px] mb-[42px] mt-[10px] h-full w-full md:px-[88px] md:mb-[106px] md:mt-[66px]">
+        <div className="mb-[42px] mt-[10px] h-full w-full px-[28px] md:mb-[106px] md:mt-[66px] md:px-[88px]">
           <Image
             src="/assets/magazine/PC_magazine_banner.svg"
             alt="매거진 PC 배너"
             width={1264}
             height={478}
-            className="hidden object-cover md:block"
+            className="hidden w-full object-cover md:block"
           />
           <Image
             src="/assets/magazine/MO_magazine_banner.svg"
@@ -25,7 +25,7 @@ const MagazinePage = () => {
             className="block w-full object-cover md:hidden"
           />
         </div>
-        <div className="px-[28px] mt-20 h-full w-full md:px-[88px]">
+        <div className="mt-20 h-full w-full px-[28px] md:px-[88px]">
           <div className="font-['Recipekorea'] text-[20px] font-medium text-sub-gray-500 md:text-[32px]">
             지금 꼭 봐야하는 매거진
           </div>
