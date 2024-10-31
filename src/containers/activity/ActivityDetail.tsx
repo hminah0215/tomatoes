625138ba05d25fef190e9b575c826b97fb26adad
starcradle101 <dreamerdev12@gmail.com>
2024-10-31 21:55:04 +0900

    feat: formatDate를 활용한 공모전 및 대외활동 상세페이지 날짜 표시 변경
    Related to: #68

--- src/containers/activity/ActivityDetail.tsx
+++ src/containers/activity/ActivityDetail.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Dday from '@/components/common/Dday';
 import ActivityContestDetailTab from '@/components/common/ActivityContestDetailTab';
 import ActivityContestDescription from '@/components/common/ActivityContestDescription';
+import { formatDate } from '@/utils/format';
 import { useState } from 'react';
 
 type ActivityContestDetailProps = {
@@ -38,10 +39,17 @@ export default function ActivityDetail({
   duration,
 }: ActivityContestDetailProps) {
   const [activeTab, setActiveTab] = useState('상세내용');
+  const [formatted_start_date, formatted_end_date] = [
+    formatDate(start_date),
+    formatDate(end_date),
+  ];
 
   const details = [
     { label: '참여 분야', value: field },
-    { label: '공모 기간', value: `${start_date} ~ ${end_date}` },
+    {
+      label: '공모 기간',
+      value: `${formatted_start_date} ~ ${formatted_end_date}`,
+    },
     { label: '주최 기관', value: company },
     { label: '활동 기간', value: duration },
     {
