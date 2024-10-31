e5de5bb74e7e0575f03690b5f75c56a188817e5a
kitaePark <bkt4044@naver.com>
2024-10-31 16:07:08 +0900

    feat: 소개 페이지 준비중 이미지 추가 및 SideBar 수정

--- src/containers/cs/LegalNotice.tsx
+++ src/containers/cs/LegalNotice.tsx
@@ -1,14 +1,25 @@
-export default function LegalNotice() {
+import Image from 'next/image';
+
+export default function AdInquiry() {
   return (
     <>
       <div>
         {/* 상단 타이틀 */}
         <div className="mt-[88px] h-[72px] text-5xl font-bold text-sub-gray-500">
-          책임한계와 법적고지
+          책임한계와 법적고지
         </div>
 
         {/* 구분선 */}
         <div className="mb-[40px] mt-4 h-[1px] w-[999px] bg-sub-gray-200" />
+
+        <div className="relative flex h-[295px] w-[319px] items-center justify-center rounded-[20px] p-4 md:h-[295px] md:w-[999px] md:p-8">
+          <Image
+            src="/assets/magazine/PC_report_tomato.svg"
+            alt="토마토 리포트"
+            width={168}
+            height={271}
+          />
+        </div>
       </div>
     </>
   );
