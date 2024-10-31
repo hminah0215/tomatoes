4140a75f009938a2c4c5790514d7df399ea314ca
hminah0215 <hyeonminah@gmail.com>
2024-10-31 19:39:58 +0900

    feat: 페이지이동시 아이템 위치로 스크롤이동 #67

--- src/containers/magazine/MtomatoPick.tsx
+++ src/containers/magazine/MtomatoPick.tsx
@@ -1,15 +1,27 @@
 // app/magazine/MtomatoPick.tsx
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Pagination from '@/components/ui/pagination/Pagination';
 import HomeGridItem from '@/containers/home/HomeGridItem';
 import { fetchBestPickAll } from '@/lib/fetchBestPickGridView';
+import { useSearchParams } from 'next/navigation';
 
 const MtomatoPick = () => {
   const [tomatoPicks, setTomatoPicks] = useState<ContestActivityDataProps[]>(
     []
   );
+
+  // "토마토 Pick" 제목 위치를 참조하기 위한 ref
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  // url 쿼리파라미터 가져오기
+  // 현재 페이지네이션의 페이지 번호, 쿼리 파라미터 'page'를 참조, 기본값은 '1'
+  const searchParams = useSearchParams();
+  const currentPage = searchParams.get('page') || '1';
+
+  // 컴포넌트가 처음 로드될 때를 확인하는 상태, 페이지네이션 변경 시 스크롤 방지에 사용
+  const [isInitialLoad, setIsInitialLoad] = useState(true);
 
   useEffect(() => {
     const fetchPicks = async () => {
@@ -28,9 +40,21 @@ const MtomatoPick = () => {
     fetchPicks();
   }, []);
 
+  // 페이지 변경 시  "토마토 Pick" 제목 위치로 스크롤 이동 (처음 로드 시에는 제외)
+  useEffect(() => {
+    if (!isInitialLoad) {
+      titleRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      setIsInitialLoad(false);
+    }
+  }, [currentPage]);
+
   return (
     <>
-      <div className="font-['Recipekorea'] text-[20px] font-medium text-sub-gray-500 md:text-[32px]">
+      <div
+        ref={titleRef}
+        className="font-['Recipekorea'] text-[20px] font-medium text-sub-gray-500 md:text-[32px]"
+      >
         토마토 Pick
       </div>
 
