f4c1d4dc54db00c440c0541a885e418c665ecf4f
kitaePark <bkt4044@naver.com>
2024-10-31 17:48:48 +0900

    design: 매거진 토마토 썸네일 추가

--- src/containers/search/SearchResults.tsx
+++ src/containers/search/SearchResults.tsx
@@ -1,18 +1,27 @@
-// components/SearchResults.tsx
-import NoResult from '@/components/common/noResult';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function SearchResults({ results, query }: SearchResultsProps) {
-  if (results.length === 0) {
-    return <NoResult searchKeyword={query} />;
-  }
-
+  // 검색 결과 클릭시 페이지 이동
   const getResultHref = (result: SearchResult) => {
     if (result.link) return `/magazine/tomatoTip/${result.id}`;
     if (result.main_category === '공모전') return `/contest/${result.id}`;
     if (result.main_category === '대외활동') return `/activity/${result.id}`;
     return '#';
+  };
+
+  // 썸네일 URL 설정 함수
+  const getThumbnailUrl = (result: SearchResult, index: number) => {
+    const thumbnailImages = [
+      `/assets/magazine/PC_tips_thumbnail_1.svg`,
+      `/assets/magazine/PC_tips_thumbnail_2.svg`,
+      `/assets/magazine/PC_tips_thumbnail_3.svg`,
+    ];
+
+    // Supabase 썸네일 URL이 없으면 순차적 썸네일 이미지 제공
+    return (
+      result.thumbnail_url || thumbnailImages[index % thumbnailImages.length]
+    );
   };
 
   return (
@@ -23,23 +32,25 @@ export default function SearchResults({ results, query }: SearchResultsProps) {
           대한 검색 결과 총 {results.length}건
         </div>
         <div className="w-full items-center justify-between rounded-[20px] border border-sub-gray-100 px-[48px]">
-          {results.map((result) => (
+          {results.map((result, index) => (
             <Link key={result.id} href={getResultHref(result)}>
               <div className="flex items-center gap-6 border-b-[1px] py-[32px]">
-                <Image
-                  src={result.thumbnail_url || '/assets/test_image.png'}
-                  alt="Search Image"
-                  width={140}
-                  height={168}
-                />
-                <div className="flex flex-col items-start gap-3 pl-[14px]">
+                <div className="flex h-[168px] w-[140px] overflow-hidden">
+                  <Image
+                    src={getThumbnailUrl(result, index)}
+                    alt="Search Image"
+                    width={140}
+                    height={168}
+                  />
+                </div>
+                <div className="flex h-[158px] flex-col items-start gap-3 pl-[14px]">
                   <div className="flex">
                     <div>공모전</div>
                     <div className="ml-[12px] text-[20px] font-medium text-sub-gray-400">
                       {result.company}
                     </div>
                   </div>
-                  <div className="text-[32px] font-semibold text-sub-gray-500">
+                  <div className="h-[76px] text-[32px] font-semibold text-sub-gray-500">
                     {result.title}
                   </div>
                   <div className="text-xl font-medium text-sub-gray-300">
