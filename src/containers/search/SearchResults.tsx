844f4135d59dc4f7d502bb643cb0023108e02683
kitaePark <bkt4044@naver.com>
2024-11-12 17:28:43 +0900

    refactor: getTagLabel 유틸함수 생성해서 검색 결과창에 뱃지 조건 생성

--- src/containers/search/SearchResults.tsx
+++ src/containers/search/SearchResults.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import SearchDdayDisplay from './SearchDdayDisplay';
 import Tag from '@/components/common/Tag';
+import { getTagLabel } from '@/utils/getTagLabel';
 
 export default function SearchResults({ results, query }: SearchResultsProps) {
   // 검색 결과 클릭시 페이지 이동
@@ -34,55 +35,58 @@ export default function SearchResults({ results, query }: SearchResultsProps) {
           대한 검색 결과 총 {results.length}건
         </div>
         <div className="relative w-full items-center justify-between rounded-[20px] border border-sub-gray-100 px-[48px]">
-          {results.map((result, index) => (
-            <Link key={result.id} href={getResultHref(result)}>
-              <div className="flex items-center gap-6 border-b-[1px] py-[32px]">
-                <div className="flex h-[168px] w-[140px] overflow-hidden">
-                  <Image
-                    src={getThumbnailUrl(result, index)}
-                    alt="Search Image"
-                    width={140}
-                    height={168}
-                  />
-                </div>
-                <div className="flex h-[158px] flex-col items-start gap-3 pl-[14px]">
-                  <div className="flex">
-                    <div className="inline-flex h-[38px] items-center justify-center gap-2.5 rounded-[100px] bg-sub-gray-500 px-3 py-1 shadow">
-                      <div className="text-center text-xl font-medium leading-[30px] text-main-white">
-                        {result.main_category === '공모전' && '공모전'}
-                        {result.main_category === '대외활동' && '대외활동'}
-                        {result.link && '매거진'}
+          {results.map((result, index) => {
+            const tagLabel = getTagLabel(result.views, result.created_at);
+            return (
+              <Link key={result.id} href={getResultHref(result)}>
+                <div className="flex items-center gap-6 border-b-[1px] py-[32px]">
+                  <div className="flex h-[168px] w-[140px] overflow-hidden">
+                    <Image
+                      src={getThumbnailUrl(result, index)}
+                      alt="Search Image"
+                      width={140}
+                      height={168}
+                    />
+                  </div>
+                  <div className="flex h-[158px] flex-col items-start gap-3 pl-[14px]">
+                    <div className="flex">
+                      <div className="inline-flex h-[38px] items-center justify-center gap-2.5 rounded-[100px] bg-sub-gray-500 px-3 py-1 shadow">
+                        <div className="text-center text-xl font-medium leading-[30px] text-main-white">
+                          {result.main_category === '공모전' && '공모전'}
+                          {result.main_category === '대외활동' && '대외활동'}
+                          {result.link && '매거진'}
+                        </div>
+                      </div>
+                      <div className="ml-[12px] text-[20px] font-medium text-sub-gray-400">
+                        {result.company}
                       </div>
                     </div>
-                    <div className="ml-[12px] text-[20px] font-medium text-sub-gray-400">
-                      {result.company}
+                    <div className="flex h-[76px] text-[32px] font-semibold text-sub-gray-500">
+                      {result.title}
+                    </div>
+                    <div className="text-xl font-medium text-sub-gray-300">
+                      조회{' '}
+                      {(
+                        (result.main_category === '공모전' ||
+                        result.main_category === '대외활동'
+                          ? result.view_count
+                          : result.views) ?? 0
+                      ).toLocaleString()}
+                      회
                     </div>
                   </div>
-                  <div className="flex h-[76px] text-[32px] font-semibold text-sub-gray-500">
-                    {result.title}
-                  </div>
-                  <div className="text-xl font-medium text-sub-gray-300">
-                    조회{' '}
-                    {(
-                      (result.main_category === '공모전' ||
-                      result.main_category === '대외활동'
-                        ? result.view_count
-                        : result.views) ?? 0
-                    ).toLocaleString()}
-                    회
+                  <div className="absolute right-[48px]">
+                    {result.main_category === '공모전' ||
+                    result.main_category === '대외활동' ? (
+                      <SearchDdayDisplay d_day={result.d_day} />
+                    ) : (
+                      <Tag type={tagLabel} label={tagLabel.toUpperCase()} />
+                    )}
                   </div>
                 </div>
-                <div className="absolute right-[48px]">
-                  {result.main_category === '공모전' ||
-                  result.main_category === '대외활동' ? (
-                    <SearchDdayDisplay d_day={result.d_day} />
-                  ) : (
-                    <Tag type="hot" label="HOT" />
-                  )}
-                </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            );
+          })}
         </div>
       </div>
     </>
